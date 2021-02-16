c3a0a21132fbbaa74f9ff9fc3c5c86f49cde079c
Jayaprabu Jayapal <j.jayaprabu@gmail.com>
2021-02-16 18:21:04 +0000

    added auth

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome to your Smart Store</h1>
+        <h1>Welcome to your Smart Store with Auth</h1>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
